c45c50dc83d3a6e830ce797f0fc6bc58af7d8608
Vikramaditya singh bhati <33588312+6-god@users.noreply.github.com>
2018-05-16 22:55:43 +0530

    spider to scrape quotes

---
+++ quotes_spider/spiders/quotes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        #h1_tag=response.xpath('//h1/a/text()').extract()
+        #tags=response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        #yield{'H1 tag':h1_tag,'Tags':tags}
+        quotes=response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text=quote.xpath(".//*[@class='text']/text()").extract_first()
+            author=quote.xpath(".//*[@class='author']/text()").extract_first()
+            tags=quote.xpath(".//*[@class='tag']/text()").extract_first()
+            yield{"text":text,"author":author,"tags":tags}
+
+
+        next_page_url=response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_page=response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page)
